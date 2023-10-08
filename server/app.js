c3152af247148b4aa9f91cad46b012d25511e588
bcloutier412 <brandoncloutier412@gmail.com>
2023-10-08 15:50:10 -0700

    Created Client and Server App structure. Downloading dependencies

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const config = require("./utils/config.js");
+const logger = require("./utils/logger.js");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require('path');
+const { requestLogger } = require("./utils/middleware")
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+// Connecting to database
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info("connected to MongoDB");
+    })
+    .catch((error) => {
+        logger.error("error connection to MongoDB:", error.message);
+    });
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
+module.exports = app;
