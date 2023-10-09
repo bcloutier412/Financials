f6cd42e72139bd855b142bece8db4d8ee90fc828
bcloutier412 <brandoncloutier412@gmail.com>
2023-10-09 12:39:03 -0700

    created user and asset schema

---
+++ server/models/asset.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const assetSchema = new mongoose.Schema({
+  name: String,
+  assetClass: String,
+  currentPrice: Number,
+  buyInPrice: Number,
+  quantity: Number,
+  purchaseDate: Date,
+  notes: []
+})
+
+assetSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const Asset = mongoose.model('Asset', assetSchema)
+
+module.exports = {
+  Asset,
+  assetSchema
+}
