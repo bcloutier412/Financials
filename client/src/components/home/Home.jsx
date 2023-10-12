22047dad5b895c4a3fd98d62e12560c5b473c98f
bcloutier412 <brandoncloutier412@gmail.com>
2023-10-12 16:41:41 -0700

    added navigate to fetchUser function

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 // Components
 import SideNav from './SideNav'
@@ -7,10 +8,16 @@ import Nav from './Nav'
 import Dashboard from './Dashboard'
 
 const Home = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchUser = async () => {
-      const result = await axios.get("/api/auth/testing")
-      console.log(result)
+      try {
+        const result = await axios.get("/api/auth/testing")
+        console.log(result)
+      } catch (error) {
+        console.log(error)
+        return navigate("/login")
+      }
     }
     fetchUser()
   })
