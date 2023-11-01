4b6015c4949436c284b671113fd5a8c6c4f3a61d
bcloutier412 <brandoncloutier412@gmail.com>
2023-10-31 23:44:26 -0700

    changed it back to production

--- client/src/api/apiUtils.js
+++ client/src/api/apiUtils.js
@@ -1,7 +1,7 @@
 const baseURL = "https://financial-tracker-api.vercel.app"
 
 const generateURL = (urlPath) => {
-  if (process.env.NODE_ENV === "Production") {
+  if (process.env.NODE_ENV === "production") {
     return baseURL + urlPath
   }
   return urlPath
